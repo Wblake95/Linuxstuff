5a42a9b7935e89534b8da65bc2936fd1e644a69a
Wesley Blake <wesleyblake95@gmail.com>
2022-07-19 18:07:38 -0700

    simple script to set up git config

---
+++ bashscriptpractice.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+echo "Hello World!"
+
+sudo pacman -Syu
+
+echo "#################"
+echo "#### Done! ######"
+echo "#################"
---
+++ gitsetup.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+git config --global user.name "Wesley Blake"
+git config --global user.email "wesleyblake95@gmail.com"
+sudo git config --system core.editor vim
+git config --global credential.helper cache
+git config --global credential.helper "cache --timeout=25000"
+git config --global push.default simple
