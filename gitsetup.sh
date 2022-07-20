a51ee8afa9ddd2b6b46108d1f566c3dbb0f8ab1c
Wesley Blake <wesleyblake95@gmail.com>
2022-07-19 18:34:35 -0700

    update git setup script

--- gitsetup.sh
+++ gitsetup.sh
@@ -2,6 +2,7 @@
 
 git config --global user.name "Wesley Blake"
 git config --global user.email "wesleyblake95@gmail.com"
+sudo pacman -S vim --needed
 sudo git config --system core.editor vim
 git config --global credential.helper cache
 git config --global credential.helper "cache --timeout=25000"
